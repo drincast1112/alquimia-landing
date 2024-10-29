0a3453de7a6c30f1bd9f4a78be5d65f3a2f1f12a
juan.hernandez <juan.hernandez@macroproyectos.com>
2024-10-29 09:06:20 -0500

    deploy ng-pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -32,7 +32,7 @@ import { LandingComponent } from './componentes/landing/landing.component';
     FormsModule, ReactiveFormsModule,
     ButtonModule
   ],
-  providers: [ PhotoService],
+  providers: [{provide: LocationStrategy,useClass: HashLocationStrategy}, PhotoService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
