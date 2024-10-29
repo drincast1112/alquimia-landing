1b0810f7aa84c67f7c757c3e8f530b815d62d081
juan.hernandez <juan.hernandez@macroproyectos.com>
2024-10-29 08:32:37 -0500

    mobile

--- src/app/componentes/landing/landing.component.ts
+++ src/app/componentes/landing/landing.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.scss']
 })
-export class LandingComponent {
+export class LandingComponent implements OnInit {
+  claseTotal: string = "bee-col bee-col-2 bee-col-w6";
+  claseContenedor: string ="bee-row-content";
+  claseBoton: string = "text-align: end";
+  constructor(){
 
+  }
+  ngOnInit(){
+    if(navigator.userAgent.match(/iPhone|iPod/i)){
+      this.claseTotal = "bee-col bee-col-2 bee-col-w12";
+      this.claseContenedor = "";
+      this.claseBoton= "text-align: center";
+    }
+
+  }
 }
