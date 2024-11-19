08ef8ae162d1e0dc8ade6f79bdaa9a8a3a4476fa
juan.hernandez <juan.hernandez@macroproyectos.com>
2024-11-18 19:52:04 -0500

    ultimo cambio

--- src/app/componentes/landing/landing.component.ts
+++ src/app/componentes/landing/landing.component.ts
@@ -7,15 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingComponent implements OnInit {
   claseTotal: string = "bee-col bee-col-2 bee-col-w6";
+  claseTotal2: string = "bee-col bee-col-1 bee-col-w7";
+  claseTotal3: string = "bee-col bee-col-2 bee-col-w5";
   claseContenedor: string ="bee-row-content";
   claseBoton: string = "text-align: end";
+  celular: boolean = false;
+
+
   constructor(){
 
   }
   ngOnInit(){
     if(navigator.userAgent.match(/iPhone|iPod/i)){
+      this.celular = true;
       this.claseTotal = "bee-col bee-col-2 bee-col-w12";
       this.claseContenedor = "";
+      this.claseTotal2 = "bee-col bee-col-1 bee-col-w12";
+      this.claseTotal3 = "bee-col bee-col-2 bee-col-w12";
       this.claseBoton= "text-align: center";
     }
 
